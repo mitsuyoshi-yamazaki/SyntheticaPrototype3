dd250b5013ff501d887c461bd360d9f887a985b7
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2025-08-11 22:53:07 +0900

    アドレス計算を修正

--- src/engine/vm-executor.ts
+++ src/engine/vm-executor.ts
@@ -481,25 +481,25 @@ export const InstructionExecutor = {
       // 相対アドレス
       case "LOAD_A":
         if (decoded.operands.offset16 !== undefined) {
-          address = (vm.pc + decoded.length + decoded.operands.offset16) & 0xffff
+          address = vm.pc + (decoded.operands.offset16 ?? 0)
           vm.setRegister("A", vm.readMemory8(address))
         }
         break
       case "STORE_A":
         if (decoded.operands.offset16 !== undefined) {
-          address = (vm.pc + decoded.length + decoded.operands.offset16) & 0xffff
+          address = vm.pc + (decoded.operands.offset16 ?? 0)
           vm.writeMemory8(address, vm.getRegister("A") & 0xff)
         }
         break
       case "LOAD_A_W":
         if (decoded.operands.offset16 !== undefined) {
-          address = (vm.pc + decoded.length + decoded.operands.offset16) & 0xffff
+          address = vm.pc + (decoded.operands.offset16 ?? 0)
           vm.setRegister("A", vm.readMemory16(address))
         }
         break
       case "STORE_A_W":
         if (decoded.operands.offset16 !== undefined) {
-          address = (vm.pc + decoded.length + decoded.operands.offset16) & 0xffff
+          address = vm.pc + (decoded.operands.offset16 ?? 0)
           vm.writeMemory16(address, vm.getRegister("A"))
         }
         break
